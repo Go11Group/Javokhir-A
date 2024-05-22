122f449b3d69a1359771d2209e4d8e594860bfea
Javohir-A <abdusamatovjavohir@gmail.com>
2024-05-23 00:16:01 +0500

    adding excel file in go

---
+++ ExcelFileInGo/structs/person.go
@@ -0,0 +1,9 @@
+package structs
+
+type Person struct {
+	Id        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
+}
