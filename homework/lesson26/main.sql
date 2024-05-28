385782431312c639e5b894a5fb4f783233ebef6b
Javohir-A <abdusamatovjavohir@gmail.com>
2024-05-28 16:10:46 +0500

    adding lesson26

---
+++ homework/lesson26/main.sql
@@ -0,0 +1,172 @@
+create table department
+(
+    id   uuid primary key default gen_random_uuid() not null,
+    name varchar                                    not null
+);
+
+
+create table if not exists employee
+(
+    id            uuid primary key default gen_random_uuid() not null,
+    name          varchar not null,
+    department_id uuid references department (id),
+    salary        int     not null default 0
+);
+
+
+create table if not exists task
+(
+    id          uuid primary key default gen_random_uuid() not null,
+    number      int unique                                 not null,
+    description text             default ''
+);
+
+create table if not exists employee_task
+(
+    id          uuid primary key default gen_random_uuid() not null,
+    employee_id uuid references employee (id),
+    task_id     uuid references task (id)
+);
+
+insert into department(id, name) values('5939ed2c-4b4b-43ae-a0ab-695175c3a731','HR');
+insert into department(id, name) values('558f6658-7793-4107-8930-cbf5dbd2b008','Programming');
+insert into department(id, name) values('08894581-f43e-4713-bccd-4283b53fd08b','Marketing');
+
+INSERT INTO employee (name, department_id, salary) VALUES
+   ('John Doe', '5939ed2c-4b4b-43ae-a0ab-695175c3a731', 50000),
+   ('Alice Smith', '558f6658-7793-4107-8930-cbf5dbd2b008', 60000),
+   ('Bob Johnson', '558f6658-7793-4107-8930-cbf5dbd2b008', 55000),
+   ('Emily Davis', '08894581-f43e-4713-bccd-4283b53fd08b', 52000),
+   ('Michael Brown', '08894581-f43e-4713-bccd-4283b53fd08b', 62000),
+   ('Jennifer Lee', '08894581-f43e-4713-bccd-4283b53fd08b', 58000),
+   ('David Wilson', '5939ed2c-4b4b-43ae-a0ab-695175c3a731', 54000),
+   ('Jessica Martinez', '5939ed2c-4b4b-43ae-a0ab-695175c3a731', 67000);
+
+INSERT INTO task (id, number, description) VALUES
+   ('1d7ab001-df4e-4ac6-adf2-70cab7568c42', 101, 'Task 101 description'),
+   ('fbb3cd9f-0567-4496-b694-96de4aad5c8d', 102, 'Task 102 description'),
+   ('488fb943-37c3-47bb-b3af-1cdd5b9dbfbb', 103, 'Task 103 description');
+
+INSERT INTO employee_task (employee_id, task_id) VALUES
+     ('0a876d4f-937e-4f77-9383-825f260728b2','1d7ab001-df4e-4ac6-adf2-70cab7568c42'),
+     ('45c2aabd-143d-40a5-b053-2142ab6521f9','1d7ab001-df4e-4ac6-adf2-70cab7568c42'),
+     ('45c2aabd-143d-40a5-b053-2142ab6521f9','fbb3cd9f-0567-4496-b694-96de4aad5c8d'),
+     ('0a876d4f-937e-4f77-9383-825f260728b2','488fb943-37c3-47bb-b3af-1cdd5b9dbfbb'),
+     ('45c2aabd-143d-40a5-b053-2142ab6521f9','488fb943-37c3-47bb-b3af-1cdd5b9dbfbb');
+
+select array_agg(e.name), t.description from employee e
+ left join department d on e.department_id=d.id
+ left join employee_task et on e.id = et.employee_id
+ left join task t on t.id = et.task_id
+where t.description is not null
+group by t.description;
+
+select array_agg(e.name), d.name, avg(salary) sum from employee e
+    left join department d on d.id = e.department_id
+    group by d.id
+    order by sum
+;
+
+-- Student table
+-- course table
+-- student_course table
+-- grade table
+
+-- 1. grade tabledagi student_id va course_id o'rniga student_course tabledagi
+-- idni olishimiz darkor;
+
+-- 2. guruhdagi eng yaxshi o'qiydigan studentlarni har guruh bo'yicha chiqaring. Agarda
+-- eng yaxshi baholar bir nechta kishida bo'lsa, hammasi chiqsin.
+
+-- 3. guruhning o'rtacha bahosini har bir guruh bo'yicha chiqaring;
+
+-- 4. eng yosh o'quvchi guruh bo'yicha chiqarilsin chiqarilsin;
+-- yani gar bir guruhdan eng yosh o'quvchi(bir nechta bo'lsa har birini)
+
+-- 5. eng yaxshi o'qiydigan guruh chiqarilsin
+-- ya'ni, har bir guruhning o'rtacha bahosining eng katta(yaxshi) bo'lgani
+
+
+create table courses 
+(   
+    course_id uuid primary key default gen_random_uuid(),
+    course_name varchar not null
+);
+
+create table students
+(
+    student_id uuid primary key default gen_random_uuid(),
+    student_name varchar not null
+);
+
+create table student_course
+(
+    id uuid primary key default gen_random_uuid(),
+    student_id uuid references studens(student_id),
+    course_id uuid references courses(course_id)
+);
+
+
+create table grades
+(
+    grade_id uuid primary key default gen_random_uuid(),
+    student_course_id uuid references student_course(id),
+    grade int check (grade <= 100 and grade >= 1)
+);
+
+INSERT INTO courses (course_name)
+VALUES ('Introduction to Programming'),
+       ('Data Analysis and Visualization'),             
+       ('Web Development Fundamentals'),
+       ('Machine Learning for Beginners'),
+       ('Software Engineering Principles'),
+       ('Creative Writing Workshop'),
+       ('Public Speaking and Communication'),
+       ('Calculus I: Differentiation and Integration'),
+       ('Physics for Scientists and Engineers'),
+       ('World History: From Antiquity to the Present');
+
+INSERT INTO students (student_name)
+VALUES ('Alice Johnson'),
+       ('Bob Williams'),
+       ('Charlie Brown'),
+       ('David Miller'),
+       ('Emily Garcia'),
+       ('Frederick Anderson'),
+       ('Grace Lee'),
+       ('Henry Moore'),
+       ('Isabella Davis'),
+       ('Jacob Wilson');
+
+
+
+--               course_id               |                 course_name                  
+-- --------------------------------------+----------------------------------------------
+--  86dc19da-494e-4f9d-af41-21d8c02395c5 | Introduction to Programming
+--  6e1168b1-8724-44b1-9659-02a8bcecbaed | Data Analysis and Visualization
+--  fa2099e2-b67f-4eab-9f24-fccad64f5804 | Web Development Fundamentals
+--  0bac3f1d-1f7a-4d7d-b759-fda6c3e80b51 | Machine Learning for Beginners
+--  86edb53b-81ab-42ee-8bf7-d5c3b031d028 | Software Engineering Principles
+--  dd526c76-772d-4641-b861-bcb11c332e19 | Creative Writing Workshop
+--  06d23597-add6-4ff5-950d-b64c1efb76b3 | Public Speaking and Communication
+--  5a11344a-e95d-4624-881e-66ca9394c2fa | Calculus I: Differentiation and Integration
+--  208734ae-f4fd-45b1-9799-94b831ba7ce9 | Physics for Scientists and Engineers
+--  2b4d27e9-4b49-421c-8eb2-ed98a1094ee0 | World History: From Antiquity to the Present
+-- (10 rows)
+
+
+--               student_id              |    student_name    
+-- --------------------------------------+--------------------
+--  62e30520-26e3-405f-b8b2-386e3ff071c2 | Alice Johnson
+--  6496ce48-2260-48a2-9af9-56fbeb8d070c | Bob Williams
+--  91700992-8d1d-4559-a791-d141d1619f29 | Charlie Brown
+--  753f167f-4b3a-44f0-ac1c-6bdd8f15fc17 | David Miller
+--  015677ca-80d7-444a-8c9a-db058b623d69 | Emily Garcia
+--  8dd6afc7-04d3-4627-850c-af6b072e4623 | Frederick Anderson
+--  7eb0d90d-d29e-4e7f-b236-4d5c1643187c | Grace Lee
+--  71e0fcbc-f94b-4423-b71e-dde6c314b9c9 | Henry Moore
+--  7b321c32-79b3-42a7-916c-257e466b8737 | Isabella Davis
+--  38e4e8ed-f494-40d5-9484-57dc60cdcb54 | Jacob Wilson
+-- (10 rows)
+
+    
